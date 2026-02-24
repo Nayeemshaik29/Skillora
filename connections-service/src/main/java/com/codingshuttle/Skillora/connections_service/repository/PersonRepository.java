4e1bce427e55669a04fd49a19607a7f011a645ca
Nayeemshaik29 <bashanayeem79@gmail.com>
2026-02-24 08:30:30 +0530

    configured and build api end-points for connection-service

--- connections-service/src/main/java/com/codingshuttle/Skillora/connections_service/repository/PersonRepository.java
+++ connections-service/src/main/java/com/codingshuttle/Skillora/connections_service/repository/PersonRepository.java
@@ -14,30 +14,49 @@ public interface PersonRepository extends Neo4jRepository<Person,Long> {
 
     Optional<Person> getByName(String name);
 
+
+    /// Custom Query for Neo4J
+
     @Query("MATCH (PersonA:Person) -[:CONNECTED_TO]- (PersonB:Person) " +
             "WHERE PersonA.userId=$userId " +
             "RETURN DISTINCT PersonB;")
     List<Person> getFirstDegreeConnections(Long userId);
+
+
+
+
     @Query("MATCH (p1:Person)-[r:REQUESTED_TO]->(p2:Person) " +
             "WHERE p1.userId = $senderId AND p2.userId = $receiverId " +
             "RETURN count(r) > 0")
     boolean connectionRequestExists(Long senderId, Long receiverId);
+
+
+
 
     @Query("MATCH (p1:Person)-[r:CONNECTED_TO]-(p2:Person) " +
             "WHERE p1.userId = $senderId AND p2.userId = $receiverId " +
             "RETURN count(r) > 0")
     boolean alreadyConnected(Long senderId, Long receiverId);
 
+
+
+
     @Query("MATCH (p1:Person), (p2:Person) " +
             "WHERE p1.userId = $senderId AND p2.userId = $receiverId " +
             "CREATE (p1)-[:REQUESTED_TO]->(p2)")
     void addConnectionRequest(Long senderId, Long receiverId);
+
+
 
     @Query("MATCH (p1:Person)-[r:REQUESTED_TO]->(p2:Person) " +
             "WHERE p1.userId = $senderId AND p2.userId = $receiverId " +
             "DELETE r " +
             "CREATE (p1)-[:CONNECTED_TO]->(p2)")
     void acceptConnectionRequest(Long senderId, Long receiverId);
+
+
+
+
 
     @Query("MATCH (p1:Person)-[r:REQUESTED_TO]->(p2:Person) " +
             "WHERE p1.userId = $senderId AND p2.userId = $receiverId " +
